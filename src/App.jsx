38b1959aeb55a499424717228bc4628925359b8b
Rahmican Tut <rahmicantut052@gmail.com>
2024-06-14 01:40:05 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './components/Home.jsx';
 import Equipments from "./components/Equipments.jsx";
 import Muzikler from './components/Muzikler.jsx';
 import Anilar from './components/Anilar.jsx';
+import Page404 from './components/Page404.jsx';
 
 function App() {
     return (
@@ -14,6 +15,7 @@ function App() {
                     <Route path="/ekipmanlar" element={<Equipments />} />
                     <Route path="/anilar" element={<Anilar />} />
                     <Route path="/muzikler" element={<Muzikler />} />
+                    <Route path="*" element={<Page404 />}></Route>
                 </Routes>
             </>
     );
