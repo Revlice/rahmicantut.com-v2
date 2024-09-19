8467d4ea2a1bc5c8a7170c1657ea9ad678215a0c
Rahmican Tut <124275194+Revlice@users.noreply.github.com>
2024-09-19 23:00:37 +0300

    Update Muzikler.jsx

--- src/components/Muzikler.jsx
+++ src/components/Muzikler.jsx
@@ -7,17 +7,6 @@ const Muzikler = ()=>{
             <Header/>
             <motion.div initial={{translateY:150}} animate={{translateY:0}} className="grid place-items-center sm:flex sm:flex-col bg-slate-950 w-full ">
                 <ul className="grid grid-cols-3 sm:flex sm:flex-col  gap-28 mt-12 ">
-                    <li className="shadow-2xl shadow-amber-600"  style={{borderRadius: '12px', overflow: 'hidden'}}>
-                        <iframe
-                            style={{borderRadius: '12px'}}
-                            src="https://open.spotify.com/embed/playlist/0Ki3HsF2xMS19sGjN7lkbL?utm_source=generator"
-                            width="500"
-                            height="500"
-                            frameBorder="0"
-                            allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                            loading="lazy"
-                        ></iframe>
-                    </li>
                     <li className="shadow-2xl shadow-white/50" style={{borderRadius: '12px', overflow: 'hidden'}}>
                         <iframe
                             style={{borderRadius: '12px'}}
